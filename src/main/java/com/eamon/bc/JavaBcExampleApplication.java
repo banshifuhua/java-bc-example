305bbece40248e78b4f026107c5d6e0f4c94fa49
eamon.zhang <1qaz@WSX#EDC>
2019-01-10 14:51:15 +0800

    初始化项目结构

---
+++ src/main/java/com/eamon/bc/JavaBcExampleApplication.java
@@ -0,0 +1,14 @@
+package com.eamon.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBcExampleApplication.class, args);
+    }
+
+}
+
